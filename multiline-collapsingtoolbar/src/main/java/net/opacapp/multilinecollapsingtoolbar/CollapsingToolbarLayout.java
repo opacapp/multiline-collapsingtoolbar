97e1bbe5d6c3d2645003df7fed631ff6d9fd4ace
Johan von Forstner <johan.forstner@gmail.com>
2016-07-16 20:47:11 +0200

    Don't check for AppCompat theme

--- multiline-collapsingtoolbar/src/main/java/net/opacapp/multilinecollapsingtoolbar/CollapsingToolbarLayout.java
+++ multiline-collapsingtoolbar/src/main/java/net/opacapp/multilinecollapsingtoolbar/CollapsingToolbarLayout.java
@@ -139,7 +139,7 @@ public class CollapsingToolbarLayout extends FrameLayout {
     public CollapsingToolbarLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        ThemeUtils.checkAppCompatTheme(context);
+        //ThemeUtils.checkAppCompatTheme(context);
 
         mCollapsingTextHelper = new CollapsingTextHelper(this);
         mCollapsingTextHelper.setTextSizeInterpolator(AnimationUtils.DECELERATE_INTERPOLATOR);
